aa72b7eac6abdbaad445b5e7add38f702df09aec
João <joaopaulomirandamatias@hotmail.com>
2021-06-25 10:16:43 -0300

    Aula 03

---
+++ payflow/lib/shared/app_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+
+import 'themes/appcolors.dart';
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Payflow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
+
+
+
+
